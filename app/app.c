cd54e556922878a6ba61a0b6afb545fae029a53d
ajin <zhouyasia@gmail.com>
2018-10-09 23:12:41 +0800

    list example

--- app/app.c
+++ app/app.c
@@ -1,7 +1,7 @@
 /*
  * app.c
  *
- *  Created on: 2017年12月17日
+ *  Created on: 2017/12/17
  *      Author: asia
  */
 
@@ -12,7 +12,8 @@
 #include "log.h"
 
 #include "common.h"
-
+#include "list.h"
+#include "buffer.h"
 int main(){
 
     int aa1 = 1;
@@ -20,5 +21,35 @@ int main(){
     SWAP(aa1, bb1);
     printf("aa=%d,bb=%d\n",aa1, bb1);
 
+//>>>>>>>>>>>>>>>>>>>> list >>>>>>>>>>>>>>>>>>>>>>>
+    list_t *langs = list_new();
+
+    list_node_t *c = list_rpush(langs, list_node_new("c"));
+    list_node_t *js = list_rpush(langs, list_node_new("js"));
+    list_node_t *ruby = list_rpush(langs, list_node_new("ruby"));
+
+    list_node_t *node;
+    list_iterator_t *it = list_iterator_new(langs, LIST_HEAD);
+    while ((node = list_iterator_next(it))) {
+      puts(node->val);
+    }
+
+    list_iterator_destroy(it);
+    list_destroy(langs);
+//<<<<<<<<<<<<<<<<<<<<<< list <<<<<<<<<<<<<<<<<<<<<
+
+//>>>>>>>>>>>>>>>>>>>>>> string >>>>>>>>>>>>>>>>>>>
+//    char *s = "hello";
+//    buffer_t * str = buffer_new_with_string(s);
+//    if(str == NULL){
+//    	printf("NULL\n");
+//    	return ;
+//    }else {
+//    	printf("not \n");
+//    }
+//    printf(" %s fdsa\n",str->data);
+//    buffer_append(str, "world");
+////    buffer_print(str);
+//<<<<<<<<<<<<<<<<<<<<<< string <<<<<<<<<<<<<<<<<<<
 	return 0;
 }
