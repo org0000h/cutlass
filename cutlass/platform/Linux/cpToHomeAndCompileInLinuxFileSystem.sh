a6244e208d9584ec83cf69323852ceeec373bdc2
ajin <zhouyasia@gmail.com>
2019-01-03 23:49:19 +0800

    cmakelists modify

--- cutlass/platform/Linux/cpToHomeAndCompileInLinuxFileSystem.sh
+++ cutlass/platform/Linux/cpToHomeAndCompileInLinuxFileSystem.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 rm -rf ~/cutlass/cutlass/src
-cp -r  ../../src ~/cutlass/cutlass
+rm -rf ~/cutlass/cutlass/platform/Linux
+cp -r  ../../src ~/cutlass/cutlass/
+cp -r  ../../platform/Linux ~/cutlass/cutlass/platform/
 cd ~/cutlass/cutlass/platform/Linux/
 ./build.sh
